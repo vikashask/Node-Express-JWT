a16ad690ec36bd83213d776365bab09351338dd3
vikash verma <vikashraj144@gmail.com>
2018-08-07 18:15:21 +0530

    middleware added

--- server.js
+++ server.js
@@ -33,6 +33,39 @@ app.get('/', welcome.welcome);
 app.post('/authenticate', auth.authenticate);
 
 
+// route middleware to authenticate and check token
+app.use(function (req, res, next) {
+
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+	// decode token
+	if (token) {
+
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('secrectKey'), function (err, decoded) {
+			if (err) {
+				return res.json({
+					success: false,
+					message: 'Failed to authenticate token.'
+				});
+			} else {
+				// if everything is good, save to request for use in other routes
+				req.decoded = decoded;
+				next();
+			}
+		});
+	} else {
+
+		// if there is no token return an error
+		return res.status(403).send({
+			success: false,
+			message: 'No token provided.'
+		});
+
+	}
+
+});
 
 // start the server 
 app.listen(port);
